321bc62d14d14a3a19432793e64bf2ba388a418e
Davide Maran <davidemaranzfc@gmail.com>
2023-05-10 16:43:36 +0200

    minor modifications

--- classes/lipschitz_env.py
+++ classes/lipschitz_env.py
@@ -49,30 +49,34 @@ class Lipschitz_Environment:
 
     def generate_curves(self, curve):
 
+        # Cinfty even
         if (curve == 'gaussian'):
             sigma = 0.3
             self.y = (1/2*np.pi*sigma**2)**0.5*np.exp(-(self.x)**2/(2*sigma**2))
 
+        # even sinusoidal
         elif (curve == 'cosine'):
             self.y = np.cos(np.pi*self.x)
 
-        elif (curve == 'poly'):
+        # polynomial even
+        elif (curve == 'even_poly'):
             z1 = -0.75
             z2 = 0.75
             self.y = -(self.x - z1)**2*(self.x - z2)**2
 
-        elif (curve == 'randpoly'):
-            a = np.random.normal(0,1)
-            b = np.random.normal(0,1)
-            c = np.random.normal(0,1)
+        # generic polynomial
+        elif (curve == 'poly'):
+            a = -1
+            b = 1
+            c = -1
             self.y = a*self.x**2 + b*self.x + c
 
-
+        # multimodal not even function
         elif (curve == 'sin-like'):
             for i in range(self.n_arms):
-                self.y[i] = (self.x[i]/self.lim)*np.cos(self.x[i])**2
+                self.y[i] = ((self.x[i]-0.2)/self.lim)*np.sin(5*self.x[i])
 
-
+        # very ugly even function
         elif (curve == 'spike'):
             spike_par = 0.5
             mean_x = self.x[self.n_arms//2]
@@ -80,10 +84,10 @@ class Lipschitz_Environment:
                 if (abs(self.x[i]-mean_x)<spike_par):
                     self.y[i] = spike_par - abs(self.x[i]-mean_x)
 
-
+        # ugly and not symmetric
         elif (curve == 'random'):
             for i in range(1,self.n_arms):
-                self.y[i] = np.clip(self.y[i-1] + np.random.uniform(-self.h, self.h), 0,1)
+                self.y[i] = self.y[i-1] + 10*np.random.uniform(-self.h, self.h)
 
         elif (curve == 'symrandom'):
             for i in range(1,int(self.n_arms//2)+1):
---
+++ main4.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+from classes.lipschitz_env import *
+from classes.lipschitz_env import Lipschitz_Environment
+from classes.lipschitz_env import Lipschitz_Environment
+from classes.fourierucb import FourierUCB
+from classes.legendreucb import LegendreUCB
+from classes.chebishevucb import ChebishevUCB
+from classes.baselines.learners import UCB1
+from classes.baselines.lips_learners import ZOOM
+from classes.baselines.advanced_learners import IGP_UCB, Gauss_Bandit
+from classes.baselines.GPUCB import GPUCB
+from functions.test_algorithm import test_algorithm
+from functions.confidence_bounds import bootstrap_ci
+from functions.plot_from_dataset import plot_data
+import os
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import datetime
+from joblib import Parallel, delayed
+import time
+import json
+
+
+
+curves = ['gaussian', 'cosine', 'even_poly', 'poly', 'sin-like', 'spike', 'random']
+
+for curve in curves:
+    tail = datetime.datetime.now().strftime("%y_%m_%d-%H_%M_")
+    dir = '/data/davide/Linear_endless_bandits/'+'_'+tail+'X'+curve
+
+    os.mkdir(dir)
+    dir = dir+'/'
+
+
+    print("-----------------------------------------------------------\n")
+    print("Linear Endless Bandit Performances on Synthetic Data\n")
+    print("-----------------------------------------------------------\n")
+
+
+    env = Lipschitz_Environment(lim=1.0, sigma=1.0, curve = curve, n_arms=100)
+
+    # save reward curve
+    np.save(dir+'reward_curve', env.y)
+
+    T = 10000
+    seeds = 20
+
+    policies = [UCB1(len(env.x)), ZOOM(env.x), GPUCB(env.x, update_every=20)]
+    labels = ['UCB1', 'ZOOM', 'GP']
+
+    ############# Fourier
+    mf = 0.1
+    df = 8
+    policies += [FourierUCB(env.x, df, T, m=mf), FourierUCB(env.x, df, T, m=mf, only_even=True)]
+    labels += ['FourierUCB', 'FourierUCB_even']
+
+    ############# Legendre
+    ml = 1.0
+    dl = 6
+    policies += [LegendreUCB(env.x, dl, T=T, m=ml), LegendreUCB(env.x, dl, T=T, m=ml, only_even=True)]
+    labels += ['LegendreUCB', 'LegendreUCB_even']
+
+    parameters = {'mf': mf, 'df': df, 'ml': ml, 'dl': dl}
+
+    ############# ChebishevUCB
+    mc = 1.0
+    dc = 6
+    policies += [ChebishevUCB(env.x, dl, T=T, m=ml), ChebishevUCB(env.x, dl, T=T, m=ml, only_even=True)]
+    labels += ['ChebishevUCB', 'ChebishevUCB_even']
+
+    parameters = {'mf': mf, 'df': df, 'ml': ml, 'dl': dl, 'mc' : mc, 'dc' : dc}
+
+    running_times = {}
+
+    results = [] 
+
+    for i in range(len(policies)):
+
+        ####################################
+        # actual algorithm simulation
+
+        # evaluate running time of the algorithm
+        t0 = time.time()
+
+        # test the algorithm
+        results.append(Parallel(n_jobs=seeds)(delayed(test_algorithm)(policies[i], env, T, seeds=1, first_seed=seed) for seed in range(seeds)))
+
+        # store time
+        t1 = time.time()
+        running_times[labels[i]] = t1 - t0
+        
+        print(labels[i] + ' finished')
+
+        ####################################
+        # part to save data
+
+        results[i] = np.concatenate(results[i], axis=0)
+
+        # make nonparametric confidence intervals
+        low, high = bootstrap_ci(results[i])
+
+        # make plot
+        # plot_data(np.arange(0,T), low, high, col='C{}'.format(i), label=labels[i])
+
+        # save data in given folder
+        np.save(dir+labels[i], results[i])
+
+
+    with open(dir+"running_times.json", "w") as f:
+        # Convert the dictionary to a JSON string and write it to the file
+        json.dump(running_times, f)
+
+    with open(dir+"parameters.json", "w") as g:
+        # Convert the dictionary to a JSON string and write it to the file
+        json.dump(parameters, g)
+
+
